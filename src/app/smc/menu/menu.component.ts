26c1303bed1cec024fd694480c71cb578d85cd5c
Juan Camilo Villada Gamboa <jncvg17@hotmail.com>
2018-05-26 17:29:26 -0500

    Bug Fixes

--- src/app/smc/menu/menu.component.ts
+++ src/app/smc/menu/menu.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
-import { AuthService } from "../services/auth.service";
+import { UserService } from '../../services/user.service';
+import { AuthService } from "../../services/auth.service";
 import { Observable } from 'rxjs/Rx';
-import { User } from '../models/user';
+import { User } from '../../models/user';
 import { Router } from "@angular/router";
 
 @Component({
