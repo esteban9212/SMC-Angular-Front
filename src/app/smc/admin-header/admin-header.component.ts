7ccd07857e52c4966e043ff87cb447e20ad4456b
Juan Camilo Villada Gamboa <jncvg17@hotmail.com>
2018-05-28 11:14:45 -0500

    Changes

--- src/app/smc/admin-header/admin-header.component.ts
+++ src/app/smc/admin-header/admin-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-admin-header',
@@ -8,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class AdminHeaderComponent implements OnInit {
 
 	nameUser : string;
+	foto : string = "https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-256.png";
 
-	constructor() { }
+	constructor(private authService: AuthService, private router: Router) { }
 
 	ngOnInit() {
 		this.nameUser = localStorage.getItem('name').slice(1, -1);
 	  	this.nameUser = this.nameUser + " " + localStorage.getItem('last_name').slice(1, -1);
+
+	var idUser = localStorage.getItem('ID_USER')
+
+    console.log(idUser);
+
+  	if(idUser === "2813"){
+  		this.foto = "https://www.icesi.edu.co/solicitud_servicios/images/img_user.php?cedula=10546071";
+  	}
+
+  	if(idUser === "5000"){
+  		this.foto = "https://conf.researchr.org/getProfileImage/norhavillegas/6b2f298e-31de-44b1-bce8-74e0ada75502/small.jpg?1507858768000";
+  	}
 	  }
+
+	logout(){
+		console.log("puto el que lo lea");
+    this.authService.logout();
+    localStorage.clear();
+    this.router.navigate(['/auth/signin']);
+ 	}
 
 }
--- src/app/smc/admin-left-side/admin-left-side.component.ts
+++ src/app/smc/admin-left-side/admin-left-side.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-admin-left-side',
   templateUrl: './admin-left-side.component.html',
@@ -8,12 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class AdminLeftSideComponent implements OnInit {
 
 	nameUser : string;
+	foto : string;
 
-  constructor() { }
+  constructor() { 
+
+    var idUser = localStorage.getItem('ID_USER')
+
+    console.log(idUser);
+    
+    if(idUser == "2813"){
+      this.foto = "https://www.icesi.edu.co/solicitud_servicios/images/img_user.php?cedula=10546071";
+    }else{
+      if(idUser == "5000"){
+      this.foto = "https://conf.researchr.org/getProfileImage/norhavillegas/6b2f298e-31de-44b1-bce8-74e0ada75502/small.jpg?1507858768000";
+       }else{
+        this.foto= "https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-256.png"
+       }
+    }
+  }
 
   ngOnInit() {
 
   	this.nameUser = localStorage.getItem('name').slice(1, -1);
+
+
   }
 
 }
